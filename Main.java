390560c3c838b5b1a2f2035e121b118bdc47d44d
Yuri Goncharov <ygpanzer@gmail.com>
2021-05-22 15:12:24 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        {
+            int x = 5;
+            System.out.println(x);
+        }
+
+        {
+            int x = 6;
+            System.out.println(x);
+        }
+
+    }
+}
